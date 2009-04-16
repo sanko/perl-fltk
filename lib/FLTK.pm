bcc0d0045b6b2472ca5297d2d7ecde734cec94a4
Sanko Robinson <sanko@cpan.org>
2009-04-16 15:45:54 -0400

    FLTK::Menu
            - Newly supported classes:
                + FLTK::Menu
            - Random fixes to xs/Group.xsi

--- lib/FLTK.pm
+++ lib/FLTK.pm
@@ -155,6 +155,7 @@ bootstrap FLTK $VERSION if !($FLTK::NOXS && $FLTK::NOXS);    # for testing
 @FLTK::ItemGroup::ISA          = qw[FLTK::Menu];
 @FLTK::LightButton::ISA        = qw[FLTK::CheckButton];
 @FLTK::LineDial::ISA           = qw[FLTK::Dial];
+@FLTK::Menu::ISA               = qw[FLTK::Group];
 
 #
 #
@@ -202,8 +203,12 @@ bootstrap FLTK $VERSION if !($FLTK::NOXS && $FLTK::NOXS);    # for testing
 @FLTK::RadioItem::ISA               = qw[FLTK::Item];
 @FLTK::ToggleItem::ISA              = qw[FLTK::Item];
 @FLTK::RadioLightButton::ISA        = qw[FLTK::LightButton];
-@FLTK::StringHierarchy::ISA = qw[FLTK::List];
-@FLTK::Share_List::ISA = qw[FLTK::List];
+@FLTK::StringHierarchy::ISA         = qw[FLTK::List];
+@FLTK::Share_List::ISA              = qw[FLTK::List];
+@FLTK::InputBrowser::ISA            = qw[FLTK::Menu];
+@FLTK::MenuBar::ISA                 = qw[FLTK::Menu];
+@FLTK::PopupMenu::ISA               = qw[FLTK::Menu];
+
 =pod
 
 
