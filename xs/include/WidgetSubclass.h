a052b18707cd6a30a7f7c329741b292672c1418e
Sanko Robinson <sanko@cpan.org>
2009-08-06 23:40:16 -0400

    Expanding xs/include/WidgetSubclass.h
            - Adding support for various constructor types:
              + AlignGroup
              + AnsiWidget
              + Group (and children)
              + FrameBox
              + FlatBox
            - Call parent destructor on destruction
            - Adding support for various draw( ) function types:
              + Button (int)
              + Slider (Rectangle, Flags, bool)
            - Don't mortalize (SV*)'d widgets when calling methods
            -

--- xs/include/WidgetSubclass.h
+++ xs/include/WidgetSubclass.h
@@ -27,41 +27,106 @@ namespace fltk {
 template< class X >
 class FL_API WidgetSubclass : public X {
 public:
-    WidgetSubclass( char * cls, int x, int y, int w, int h, const char *l = 0 )
-            : X( x, y, w, h, l ) {
+    WidgetSubclass( char * cls, int x, int y, int w, int h, const char * lbl )
+            : X( x, y, w, h, lbl ) { // Just about everything
         _class = cls;
     };
+    WidgetSubclass( char * cls, int x, int y, int w, int h, const char * lbl,
+                    uchar nbr, bool v, Flags a, uchar dw, uchar dh )
+            : X( x, y, w, h, lbl, nbr, v, a, dw, dh ) { // AlignGroup
+        _class = cls;
+    };
+    WidgetSubclass( char * cls, int x, int y, int w, int h, int defsize )
+            : X( x, y, w, h, defsize ) { // AnsiWidget
+        _class = cls;
+    };
+    WidgetSubclass( char * cls, int x, int y, int w, int h, const char * lbl,
+                    bool begin )
+            : X( x, y, w, h, lbl, begin ) { // Group
+        _class = cls;
+    };
+
+    WidgetSubclass( char * cls, char * name, int dx, int dy, int dw, int dh,
+                    char * pattern, Box * down )
+            : X( name, dx, dy, dw, dh, pattern, down ) { // FrameBox
+        _class = cls;
+    };
+
+    WidgetSubclass ( char * cls, char * name )
+            : X( name ) { // FlatBox
+        _class = cls;
+    }
+
+
     ~WidgetSubclass() {
         delete _class;
+        this->~X( );
     }
     int handle( int event ) {
         int handled = 1; /* safe to assume for now */
+        AV * args = newAV();
+        av_push( args, sv_2mortal( newSViv( event ) ) );
+        handled = _call_method( "handle", args );
+        if ( handled != 1 )
+            handled = X::handle( event );
+        return handled;
+    };
+    void draw ( ) { // Just about everything supports this one
+        int handled = 1; /* safe to assume for now */
+        AV * args = newAV();
+        av_push( args, sv_2mortal( newSViv( 52 ) ) );
+        handled = _call_method( "draw", args );
+        if ( handled != 1 )
+            this->X::draw( );
+    };
+    void draw ( int glyph_width ) {  // For fltk::Button
+        int handled = 1; /* safe to assume for now */
+        AV * args = newAV();
+        av_push( args, sv_2mortal( newSViv( glyph_width ) ) );
+        handled = _call_method( "draw", args );
+        if ( handled != 1 )
+            this->X::draw( glyph_width );
+    };
+    void draw( fltk::Rectangle * sr, Flags flags, bool slot ) { // fltk::Slider
+        int handled = 1; /* safe to assume for now */
+        AV * args = newAV();
+        SV * sv_rect = sv_newmortal();
+        sv_setref_pv( sv_rect, "FLTK::Rectangle", ( void * ) sr );
+        av_push( args, sv_2mortal( newSViv( sv_rect ) ) );
+        av_push( args, sv_2mortal( newSViv( flags ) ) );
+        av_push( args, sv_2mortal( newSViv( slot ) ) );
+        handled = _call_method( "draw", args );
+        if ( handled != 1 )
+            this->X::draw( sr, flags, slot );
+    };
+private:
+    char * _class;
+    int _call_method ( char * method, AV * args ) {
+        int retval;
         dSP;
         I32 ax;
         ENTER;
         SAVETMPS;
         PUSHMARK( SP );
-        SV * widget = sv_newmortal();
-        sv_setref_pv( widget, _class, ( void* ) this );
-        XPUSHs( widget );
-        XPUSHs( sv_2mortal( newSViv( event ) ) );
+        SV * sv_self = sv_newmortal();
+        sv_setref_pv( sv_self, _class, ( void * ) this );
+        XPUSHs( sv_self );
+        for ( int i = av_len( args ); i >= 0; i-- )
+            XPUSHs( av_shift( args )   );
         PUTBACK;
-        int count = call_method( "handle", /*G_EVAL |*/ G_SCALAR );
+        int count = call_method( method, G_KEEPERR | G_EVAL | G_SCALAR );
         SPAGAIN;
         SP -= count;
         ax = ( SP - PL_stack_base ) + 1;
         if ( ( SvTRUE( ERRSV ) ) || ( count != 1 ) || ( !SvOK( ST( 0 ) ) ) )
-            handled = 0;
+            retval = 0;
         else
-            handled = SvIV( ST( 0 ) );
+            retval = SvIV( ST( 0 ) );
         PUTBACK;
         FREETMPS;
         LEAVE;
-        return handled ? handled : this->X::handle( event );
+        return retval;
     };
-private:
-    char * _class;
 };
 }
-
 #endif // #ifndef fltk_WidgetSubclass_h
