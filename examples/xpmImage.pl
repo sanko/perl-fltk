3e557c39f4b5422302661bb42866aeb58d6501b0
Sanko Robinson <sanko@cpan.org>
2009-10-17 15:46:17 -0400

    Adding a few minor notes to xpmImage example

--- examples/xpmImage.pl
+++ examples/xpmImage.pl
@@ -10,54 +10,60 @@
 =for git $Id$
 
 =cut
-
 use strict;
 use warnings;
 use FLTK;
 
 #
 my $win = FLTK::Window->new(100, 50, 'Purr');
-$win->image(FLTK::xpmImage->new(
-                         '50 34 4 1',
-                         '  c black',
-                         'o c #ff9900',
-                         '@ c #ffffff',
-                         '# c None',
-                         '##################################################',
-                         '###      ##############################       ####',
-                         '### ooooo  ###########################  ooooo ####',
-                         '### oo  oo  #########################  oo  oo ####',
-                         '### oo   oo  #######################  oo   oo ####',
-                         '### oo    oo  #####################  oo    oo ####',
-                         '### oo     oo  ###################  oo     oo ####',
-                         '### oo      oo                     oo      oo ####',
-                         '### oo       oo  ooooooooooooooo  oo       oo ####',
-                         '### oo        ooooooooooooooooooooo        oo ####',
-                         '### oo     ooooooooooooooooooooooooooo    ooo ####',
-                         '#### oo   ooooooo ooooooooooooo ooooooo   oo #####',
-                         '####  oo oooooooo ooooooooooooo oooooooo oo  #####',
-                         '##### oo oooooooo ooooooooooooo oooooooo oo ######',
-                         '#####  o ooooooooooooooooooooooooooooooo o  ######',
-                         '###### ooooooooooooooooooooooooooooooooooo #######',
-                         '##### ooooooooo     ooooooooo     ooooooooo ######',
-                         '##### oooooooo  @@@  ooooooo  @@@  oooooooo ######',
-                         '##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######',
-                         '##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######',
-                         '##### oooooooo  @@@  ooooooo  @@@  oooooooo ######',
-                         '##### ooooooooo     ooooooooo     ooooooooo ######',
-                         '###### oooooooooooooo       oooooooooooooo #######',
-                         '###### oooooooo@@@@@@@     @@@@@@@oooooooo #######',
-                         '###### ooooooo@@@@@@@@@   @@@@@@@@@ooooooo #######',
-                         '####### ooooo@@@@@@@@@@@ @@@@@@@@@@@ooooo ########',
-                         '######### oo@@@@@@@@@@@@ @@@@@@@@@@@@oo ##########',
-                         '########## o@@@@@@ @@@@@ @@@@@ @@@@@@o ###########',
-                         '########### @@@@@@@     @     @@@@@@@ ############',
-                         '############  @@@@@@@@@@@@@@@@@@@@@  #############',
-                         '##############  @@@@@@@@@@@@@@@@@  ###############',
-                         '################    @@@@@@@@@    #################',
-                         '####################         #####################',
-                         '##################################################'
-            )
+$win->image(
+    FLTK::xpmImage->new(
+
+        # width height ncolors chars_per_pixel
+        '50 34 4 1',
+
+        # colormap
+        '  c black',
+        'o c #ff9900',
+        '@ c #ffffff',
+        '# c None',
+
+        # pixels
+        '##################################################',
+        '###      ##############################       ####',
+        '### ooooo  ###########################  ooooo ####',
+        '### oo  oo  #########################  oo  oo ####',
+        '### oo   oo  #######################  oo   oo ####',
+        '### oo    oo  #####################  oo    oo ####',
+        '### oo     oo  ###################  oo     oo ####',
+        '### oo      oo                     oo      oo ####',
+        '### oo       oo  ooooooooooooooo  oo       oo ####',
+        '### oo        ooooooooooooooooooooo        oo ####',
+        '### oo     ooooooooooooooooooooooooooo    ooo ####',
+        '#### oo   ooooooo ooooooooooooo ooooooo   oo #####',
+        '####  oo oooooooo ooooooooooooo oooooooo oo  #####',
+        '##### oo oooooooo ooooooooooooo oooooooo oo ######',
+        '#####  o ooooooooooooooooooooooooooooooo o  ######',
+        '###### ooooooooooooooooooooooooooooooooooo #######',
+        '##### ooooooooo     ooooooooo     ooooooooo ######',
+        '##### oooooooo  @@@  ooooooo  @@@  oooooooo ######',
+        '##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######',
+        '##### oooooooo @@@@@ ooooooo @@@@@ oooooooo ######',
+        '##### oooooooo  @@@  ooooooo  @@@  oooooooo ######',
+        '##### ooooooooo     ooooooooo     ooooooooo ######',
+        '###### oooooooooooooo       oooooooooooooo #######',
+        '###### oooooooo@@@@@@@     @@@@@@@oooooooo #######',
+        '###### ooooooo@@@@@@@@@   @@@@@@@@@ooooooo #######',
+        '####### ooooo@@@@@@@@@@@ @@@@@@@@@@@ooooo ########',
+        '######### oo@@@@@@@@@@@@ @@@@@@@@@@@@oo ##########',
+        '########## o@@@@@@ @@@@@ @@@@@ @@@@@@o ###########',
+        '########### @@@@@@@     @     @@@@@@@ ############',
+        '############  @@@@@@@@@@@@@@@@@@@@@  #############',
+        '##############  @@@@@@@@@@@@@@@@@  ###############',
+        '################    @@@@@@@@@    #################',
+        '####################         #####################',
+        '##################################################'
+    )
 );
 $win->show;
 exit FLTK::run;
